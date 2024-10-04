d64ff63ce56b0bcbd690623ee9489ddccabc34bd
Enea Zani <eneazani2005@gmail.com>
2024-10-04 18:16:44 +0200

    removed comments

--- src/algorithms/heapSort.js
+++ src/algorithms/heapSort.js
@@ -1,24 +1,18 @@
 export const heapSort = async (array, setArray, speed) => {
     let n = array.length;
 
-    // Costruisci l'heap (riorganizza l'array)
     for (let i = Math.floor(n / 2 - 1); i >= 0; i--) {
         await heapify(array, n, i, setArray, speed);
     }
 
-    // Estrai gli elementi dall'heap uno per uno
     for (let i = n - 1; i >= 0; i--) {
-        // Sposta la radice corrente alla fine
         [array[0], array[i]] = [array[i], array[0]];
-        setArray([...array]);  // Aggiorna l'array per visualizzare il cambiamento
+        setArray([...array]);  
         await sleep(speed);
 
-        // Richiama heapify sull'heap ridotto
         await heapify(array, i, 0, setArray, speed);
     }
 };
-
-// Funzione per ripristinare l'heap
 const heapify = async (array, n, i, setArray, speed) => {
     let largest = i;
     let left = 2 * i + 1;
